67895beec2df325d3122376a941d7090571c5c2a
zannatin-tazri <zannatintazrizan@gmail.com>
2023-06-16 10:23:30 +0600

    last commit

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const port = process.env.PORT || 5000;
 
 
 
-console.log(process.env.DB_PASS);
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3moahdm.mongodb.net/?retryWrites=true&w=majority`;
